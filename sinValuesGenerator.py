d890e6607b03dd8921aa4025d82a20cf50381c9a
rkapobel01 <rok_35@live.com.ar>
2018-04-17 17:22:29 -0300

    Degree influence test

--- sinValuesGenerator.py
+++ sinValuesGenerator.py
@@ -3,6 +3,7 @@
 import numpy as np
 from numpy.linalg import inv
 import math
+import matplotlib.pyplot as plot
 
 class SinValuesGenerator:
     multiplier = 1
@@ -23,7 +24,7 @@ class SinValuesGenerator:
     def sinValues(self):
         noises = np.random.normal(self.mu, self.sigma, self.N)
         func = lambda x, e: math.sin(self.multiplier * x) + e
-        xData = np.random.uniform(self.a, self.b, self.N)
+        xData = sorted(np.random.uniform(self.a, self.b, self.N))
         yData = [func(x, e) for x, e in zip(xData, noises)]
         return (xData, yData)
 
@@ -31,11 +32,7 @@ class SinValuesGenerator:
 
 class PolynomialRegression:
     reg = 0
-    M = 1
-    X = None
-    t = None
-    xData = None
-    yData = None
+    M = 0
     w = None
 
     def __init__(self, reg, M):
@@ -43,51 +40,80 @@ class PolynomialRegression:
         self.M = M
 
     def findW(self, data):
-        if isinstance(data, tuple) and data[0] is None or data[1] is None or len(data[0]) == 0 or len(data[0]) != len(data[1]):
-            raise ValueError("data must be a tuple and [0] and [1] must be not zero arrays with equal size.")
-            
-        self.xData = data[0]
-        self.yData = data[1]
-        self.t = np.array(self.yData)
-        func = lambda x, j: x ** j
-        self.X = np.array([[func(x, j) for j in xrange(self.M + 1)] for x in self.xData])
-        Xt = np.transpose(self.X)
-        print(len(self.xData))
-        print(self.X.shape)
-        XtX = np.dot(Xt, self.X) + np.identity(self.X.shape[1]) * self.reg
-        Xt_t = np.dot(Xt, self.t)
+        self.checkData(data)            
+        t = np.array(data[0])
+        X = self.getX(data[1])
+        Xt = np.transpose(X)
+        XtX = np.dot(Xt, X) + np.identity(X.shape[1]) * self.reg
+        Xt_t = np.dot(Xt, t)
         try:
             XtXINv = inv(XtX)
             self.w = np.dot(XtXINv, Xt_t)
         except np.linalg.LinAlgError as e:
             print(e)
 
-    def error(self):
-        self.checkW()    
-        reg_cond = reduce(lambda tot, wi: tot + (wi ** 2), self.w)
-        X_dot_w = np.dot(self.X, self.w)
-        # equals to map(lambda xi, ti: (xi - ti) ** 2, zip(X_dot_w, self.t))
-        e = sum([(xi - ti) ** 2 for xi, ti in zip(X_dot_w, self.t)])
-        return 0.5 * (e + self.reg * reg_cond)
-
-    def ems(self):
-        return math.sqrt((2 * self.error()) / self.t.shape[0])
-
     def y(self, x):
         self.checkW()
         return np.dot([x ** i for i in xrange(self.M + 1)], self.w)
+
+    def error(self, data):
+        self.checkData(data)
+        self.checkW()
+        t = np.array(data[0])
+        X = self.getX(data[1]) 
+        reg_cond = reduce(lambda tot, wi: tot + (wi ** 2), self.w)
+        X_dot_w = np.dot(X, self.w)
+        # equals to map(lambda xi, ti: (xi - ti) ** 2, zip(X_dot_w, self.t))
+        e = sum([(xi - ti) ** 2 for xi, ti in zip(X_dot_w, t)])
+        return 0.5 * (e + self.reg * reg_cond)
+
+    def ems(self, data):
+        try:
+            return math.sqrt((2 * self.error(data)) / len(data[0]))
+        except ZeroDivisionError as e:
+            print("T is a void vector:\n" + str(e))
+
+    def getX(self, data):
+        func = lambda x, j: x ** j
+        return np.array([[func(x, j) for j in xrange(self.M + 1)] for x in data])
+
+    def checkData(self, data):
+        if isinstance(data, tuple) and data[0] is None or data[1] is None or len(data[0]) == 0 or len(data[0]) != len(data[1]):
+            raise ValueError("data must be a tuple and [0] and [1] must be not zero arrays with equal size.")
 
     def checkW(self):
         if self.w is None:
             raise ValueError("Error is inf. You did not calculate the error yet.")
 
+# The scritps to make the plots and the scripts to make the analysis will be separated in different modules.
+
+def plotErrorsByDegree(degrees, errors):
+    plot.plot(degrees, errors[0], "b-o", label="Training")
+    plot.plot(degrees, errors[1], "r-o", label="Test")
+    plot.legend(loc='upper right', shadow=True)
+    plot.axes().set_title("$Degree$ $influence$")
+    plot.axes().set_xlabel("$M$")
+    plot.axes().set_ylabel("$E_{drms}$")
+    plot.show()
+
 if __name__ == "__main__":
-    svg = SinValuesGenerator(2*math.pi, 0, 1, 50, 0, 10)
-    data1 = svg.sinValues()
-    data2 = svg.sinValues()
-    pr = PolynomialRegression(0, 5)
-    pr.findW(data1)
-    print(pr.w)
-    
-    print(data1)
-    print(data2)
+    N = 10
+    svg = SinValuesGenerator(2*math.pi, 0, 1, N, 0, 10)
+    trngData = svg.sinValues()
+    testData = svg.sinValues()
+
+    trainingErrors = []
+    testErrors = []
+    for degree in xrange(N):
+        pr = PolynomialRegression(0, degree)
+        pr.findW((trngData[0], trngData[1])) # Finding w* for training data
+        #print(pr.w)
+        trngError = pr.error(trngData)
+        print("Degree: " + str(degree))
+        print("trng error: " + str(trngError))
+        trainingErrors.append(np.log(trngError))
+        testError = pr.error(testData)
+        print("test error: " + str(testError))
+        testErrors.append(np.log(testError))
+
+    plotErrorsByDegree(list(range(N)), (trainingErrors, testErrors))
