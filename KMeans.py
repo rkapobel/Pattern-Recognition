9e5343423e431a0d624e8f93d541330a5098627a
rkapobel01 <rok_35@live.com.ar>
2018-07-05 23:48:29 -0300

    K-Means algorithm cycle conditions.

--- KMeans.py
+++ KMeans.py
@@ -17,15 +17,17 @@ class KMeans:
         numOfChanges = 0
         maxIter = 100
         iter = 0
-        while numOfChanges / (K * 1.0) < 5 or iter <= maxIter:
+        while numOfChanges / (K * 1.0) > 5 or iter <= maxIter:
             for i in xrange(K):
                 self.centroids[i] = sum(clusters[i]) / (len(clusters[i]) * 1.0)
             iter += 1
-            clusters = self.updateClusters(data, K)
+            clusters, numOfChanges = self.updateClusters(data, K, clusters)
 
         self.clusters = clusters
 
-    def updateClusters(self, data, K):
+    def updateClusters(self, data, K, oldClusters):
         clusters = [[] for _ in xrange(K)]
         [clusters[np.argmin([np.linalg.norm(x - mu, ord = 2) for mu in self.centroids])].append(x) for x in data]
-        return clusters
+        numOfChanges = sum([abs(len(clusters[i]) - len(oldClusters[i])) for i in xrange(K))])
+
+        return clusters, numOfChanges
