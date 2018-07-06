26dcefadd1debc9cb6c840d15935f8dadb45cb4e
rkapobel01 <rok_35@live.com.ar>
2018-07-05 23:52:33 -0300

    K-Means algorithm cycle conditions.

--- KMeans.py
+++ KMeans.py
@@ -6,23 +6,29 @@ import random as rand
 
 class KMeans:
 
+    initialCentroids = None
     centroids = []
     clusters = None
+    totalIter = 0
 
     def calculateCentroids(self, data, K):
         for _ in xrange(K):
             self.centroids.append(rand.choice(data))
         
+        self.initialCentroids = self.centroids
         clusters = self.updateClusters(data, K)
+        
         numOfChanges = 0
         maxIter = 100
-        iter = 0
-        while numOfChanges / (K * 1.0) > 5 or iter <= maxIter:
+        numIter = 0
+        
+        while numOfChanges / (K * 1.0) > 5 or numIter <= maxIter:
             for i in xrange(K):
                 self.centroids[i] = sum(clusters[i]) / (len(clusters[i]) * 1.0)
-            iter += 1
+            numIter += 1
             clusters, numOfChanges = self.updateClusters(data, K, clusters)
 
+        self.totalIter = numIter
         self.clusters = clusters
 
     def updateClusters(self, data, K, oldClusters):
