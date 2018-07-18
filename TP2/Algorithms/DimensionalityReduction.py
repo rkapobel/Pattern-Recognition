3c5f73a192e2c3d73f2d003db6aa292d230590a4
rkapobel01 <rok_35@live.com.ar>
2018-07-18 00:01:14 -0300

    Some corrections.

--- TP2/Algorithms/DimensionalityReduction.py
+++ TP2/Algorithms/DimensionalityReduction.py
@@ -7,12 +7,16 @@ class Fisher():
     Sw = np.zeros((2, 2))
     m1 = np.zeros(2)
     m2 = np.zeros(2)
+    m = np.zeros(2)
     w = None
 
-    def findW(self, data1, data2): 
+    def findW(self, data1, data2):
+        data1 = np.array(data1)
+        data2 = np.array(data2) 
         S1 = self.calculateClass1(data1)
         S2 = self.calculateClass2(data2)
         self.Sw = S1 + S2
+        self.m = (1 / float(data1.shape[0] + data2.shape[0])) * sum(data1) + sum(data2) 
         self.w = np.dot(inv(self.Sw), self.m1 - self.m2)
         print('w: {0}'.format(self.w))
 
@@ -25,10 +29,10 @@ class Fisher():
         return S2
 
     def calculateClass(self, ci):
-        ni = len(ci)
-        ci_arr = np.array(ci)
-        mi = np.sum(ci_arr, axis = 0) / ni
-        V = [x - mi for x in ci_arr]
+        ci = np.array(ci)
+        ni = ci.shape[0]
+        mi = np.sum(ci, axis = 0) / ni
+        V = [x - mi for x in ci]
         Si = sum([np.outer(v, v) for v in V])
         return mi, Si
 
@@ -37,6 +41,7 @@ class Fisher():
 
     def classificate(self, x):
         return 0 if self.reduceDimension(x) > 0.5 * np.dot(self.w, self.m1 + self.m2) else 1
+        #return 0 if np.dot(self.w, x - self.m) > 0 else 1
 
 class MCFisher():
     W = []
@@ -44,6 +49,7 @@ class MCFisher():
     means = []
 
     def findW(self, data):
+        data = np.array(data)
         data = [self.calculateClass(ci) for ci in data]
         self.means  = [val[0] for val in data]
         m = sum([val[0] * val[1] for val in data]) / sum([val[1] for val in data])
@@ -59,8 +65,8 @@ class MCFisher():
         print('Eigen values: {0}'.format(self.eigVal))
 
     def calculateClass(self, ci):
-        nk = len(ci)
         ci = np.array(ci)
+        nk = ci.shape[0]
         mk = np.sum(ci, axis = 0) / nk
         V = [x - mk for x in ci]
         Sk = sum([np.outer(v, v) for v in V])
