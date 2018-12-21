c6979305fb212e0615d364a1dc63a90439f43c1f
rkapobel01 <rok_35@live.com.ar>
2018-12-20 21:57:29 -0300

    Found how to maximize alphas: Using gradient ascent on it.

--- TP_Final/SVM.py
+++ TP_Final/SVM.py
@@ -2,15 +2,6 @@
 import numpy as np
 from numpy.linalg import inv
 import math
- 
-class Kernel:
-
-	def __init__(self, phi, func):
-		self.phi = phi
-		self.func = func		
-		
-	def calculate(xi, xj):
-		return func(phi(xi), phi(xj))
 
 class SVM:
 
@@ -22,37 +13,40 @@ class SVM:
 		self.alphas = None
 		self.X = None
 		self.Y = None
+		self.D = None
 
 	def maximizeDualProblem(X, Y):
 		self.Y = np.array(Y)
-		self.X = np.array(X) #TODO: Apply the kernel first to X
-		D = self.X.shape
-		self.alphas = self.getRandomAlphas(D[0])
-		P = np.array([x*self.alphas*self.Y for x in self.X.T]).T
-		A = np.dot(P, P.T)
-		Max = sum(self.alphas) - 0.5*sum(sum(A))
-		MaxAux = Max
+		self.X = np.array(X)
+		self.D = self.X.shape
+		alphas = None
+		alphasAux = self.getRandomAlphas(self.D[0])
 		it = 0
-		R1 = self.alphaValidation(self.alphas) #TODO: Figure out the correct way to find the alphas
-		while (not R1) and it < self.maxIter:
-			alphasAux = self.getRandomAlphas(D[0])
-			P = np.array([x*alphasAux*self.Y for x in self.X.T]).T
-			A = np.dot(P, P.T)
-			MaxAux = sum(alphasAux) - 0.5*sum(sum(A))
-			R1 = self.alphaValidation(self.alphas)
-			if MaxAux > Max and R1:
-				Max = MaxAux
-				self.alphas = alphasAux
+		while it < self.maxIter and validAlphas(alphasAux):
+			alphas = alphasAux
+			alphasAux += self.calculateJ(alphasAux)*np.zeros((self.D[0], )) # This is gradient ascent to maximize alphas
 			it += 1
-		self.W = sum(np.dot(self.X.T, self.alphas*self.Y))
+		self.findW()
 		self.findB()
+		self.alphas = alphas
 
 		def getRandomAlphas(l):
 			return numpy.random.uniform(low=0, high=self.C, size=(l,1))
 
-		def alphaValidation(alphas):
-			np.dot(alphas, self.Y) == 0
-	
+		def validAlphas(alphas):
+			for v in alphas:
+				if v < 0 or v > self.C:
+					return False
+			return True
+
+		def calculateJ(alphas):
+			P = np.array([x*alphas*self.Y for x in self.X.T]).T
+			A = np.dot(P, P.T)
+			return sum(alphas) - 0.5*sum(sum(A))	
+
+		def findW():
+			self.W = sum(np.dot(self.X.T, self.alphas*self.Y))
+
 		def findB():
 			for i in range(self.X.shape[0]):
 				xi = self.X[i]
@@ -65,4 +59,4 @@ class SVM:
 			#TODO: throw an error?
 
 		def classificate(x):
-			return np.sign(np.dot(np.dot(X, x), self.alphas*self.Y) + self.b) #TODO: Apply the kernel to x
+			return np.sign(np.dot(np.dot(X, x), self.alphas*self.Y) + self.b)
